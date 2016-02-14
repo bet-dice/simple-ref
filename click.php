b97aaa796e4a268bdb9543c607c7efd08540b20b
bet-dice <bet-dice@malwareonlinevir.com>
2016-02-14 11:24:12 +0100

    Create click.php

---
+++ click.php
@@ -0,0 +1,19 @@
+<?php
+$server = "http://bet-dice.github.io/simple-ref";
+?>
+<script type="text/javascript">
+var adfly_id = 7095100;
+var adfly_advert = 'int';
+var adfly_protocol = 'http';
+var adfly_domain = 'adf.ly';
+var domains = ['depositfiles.com','rapidshare.com','vip-file.com'];
+var adfly_nofollow = true;
+var frequency_cap = '5';
+var frequency_delay = '5';
+var init_delay = '3';
+var popunder = true;
+</script>
+<script src="https://cdn.adf.ly/js/link-converter.js"></script>
+<script src="https://cdn.adf.ly/js/entry.js"></script>
+<meta http-equiv="refresh" content="6; URL=<?php echo $server; ?>/simpleref.php">
+<h2>Click Counted</h2>
